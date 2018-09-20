3c9d45088a3cc8e6b822fd5e23b32a5a3d3a26a7
j.rosales <j.rosales@alumnos.udg.mx>
2018-09-20 17:27:03 -0400

    Modifica

--- resources/views/bienvenida.blade.php
+++ resources/views/bienvenida.blade.php
@@ -14,7 +14,7 @@
 <div class="row">
   <div class="col-md-12">
     <div class="tile">
-      <div class="tile-body">CONTENIDO PRINCIPAL</div>
+      <div class="tile-body">CONTENIDO PRINCIPAL esto es difeente</div>
     </div>
   </div>
 </div>
